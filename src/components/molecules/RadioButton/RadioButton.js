ce456aec790e88d787c3b1afb5c30d964b0ffbb4
elvinciqueira <elvinciqueira@gmail.com>
2021-03-07 18:50:52 -0300

    refactor: radiobutton

---
+++ src/components/molecules/RadioButton/RadioButton.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled, {css} from 'styled-components'
+
+const styles = css`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  list-style: none;
+  border: 1px solid ${({theme}) => theme.colors.primary.light.color};
+  list-style: none;
+  box-sizing: border-box;
+  border-radius: ${({theme}) => theme.borderRadius};
+
+  li {
+    display: flex;
+    padding: 8px;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    text-align: center;
+
+    cursor: pointer;
+
+    &:hover {
+      background-color: #d3eaff;
+    }
+  }
+
+  li:first-child {
+    border-right: 1px solid ${({theme}) => theme.colors.primary.light.color};
+  }
+
+  li:last-child {
+    border-left: 1px solid ${({theme}) => theme.colors.primary.light.color};
+  }
+
+  ${({selectedItem}) => css`
+    li:nth-child(${Number(selectedItem)}) {
+      background-color: #d3eaff;
+    }
+  `}
+
+  span {
+    color: #404040;
+  }
+`
+
+const StyledRadioButton = styled.ul`
+  ${styles}
+`
+
+export default StyledRadioButton
--- src/components/organisms/FormCadastro/FormCadastro.js
+++ src/components/organisms/FormCadastro/FormCadastro.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import styled, {css} from 'styled-components'
 import {Form} from '@unform/web'
 
 import {Grid} from '../../atoms/Grid'
 import Field from '../../molecules/Field'
 import Text from '../../atoms/Text/Text'
 import Button from '../../atoms/Button'
+import RadioButton from '../../molecules/RadioButton'
 
 const paymentsOptions = {
   1: {
@@ -21,52 +21,6 @@ const paymentsOptions = {
     img: 'icons/paypal.svg',
   },
 }
-
-const RadioButton = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  list-style: none;
-  border: 1px solid ${({theme}) => theme.colors.primary.light.color};
-  list-style: none;
-  box-sizing: border-box;
-  border-radius: ${({theme}) => theme.borderRadius};
-
-  ${(props) => console.log(props)}
-
-  li {
-    display: flex;
-    padding: 8px;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-
-    text-align: center;
-
-    cursor: pointer;
-
-    &:hover {
-      background-color: #d3eaff;
-    }
-  }
-
-  li:first-child {
-    border-right: 1px solid ${({theme}) => theme.colors.primary.light.color};
-  }
-
-  li:last-child {
-    border-left: 1px solid ${({theme}) => theme.colors.primary.light.color};
-  }
-
-  ${({selectedItem}) => css`
-    li:nth-child(${Number(selectedItem)}) {
-      background-color: #d3eaff;
-    }
-  `}
-
-  span {
-    color: #404040;
-  }
-`
 
 function FormContent() {
   const formRef = React.useRef(null)
